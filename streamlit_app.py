885a4f6ece6f2701fa92ffe1a3bbc2b9d59377e7
SRAVAN-432 <126743729+SRAVAN-432@users.noreply.github.com>
2023-03-28 08:47:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-range egg ')
-streamlit.text('🥑🍞avocad toast)
+streamlit.text('🥑🍞avocad toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
